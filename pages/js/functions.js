a908c0e4eeb33f6f64fee0cd5817fc683a38cba9
zhimin115200 <786667916>
2017-11-30 13:12:08 +0800

    test6

--- pages/js/functions.js
+++ pages/js/functions.js
@@ -1,8 +1,12 @@
 function myFunction()
 {
 
-$.get("http://cloud.bmob.cn/5795bfceb00ba9ea/getTotalPV",function(data,status){
+
+}
+
+function onLoad(){
+	$.get("http://cloud.bmob.cn/5795bfceb00ba9ea/getTotalPV",function(data,status){
 			alert("数据: " + data.results[0].totalPV + "\n状态: " + status);
-			
+			 $("#pv").text(data.results[0].totalPV);  
 		});
 }
