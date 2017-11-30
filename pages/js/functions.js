64bd9a320032f59e41bffe07069257728575015f
zhimin115200 <786667916>
2017-11-30 13:25:09 +0800

    test7

--- pages/js/functions.js
+++ pages/js/functions.js
@@ -1,12 +1,13 @@
 function myFunction()
 {
 
-
 }
 
 function onLoad(){
 	$.get("http://cloud.bmob.cn/5795bfceb00ba9ea/getTotalPV",function(data,status){
-			alert("数据: " + data.results[0].totalPV + "\n状态: " + status);
 			 $("#pv").text(data.results[0].totalPV);  
+		});
+	$.get("http://cloud.bmob.cn/5795bfceb00ba9ea/totalPVpp",function(data,status){
+			 console.log(JSON.stringify(data)) 
 		});
 }
